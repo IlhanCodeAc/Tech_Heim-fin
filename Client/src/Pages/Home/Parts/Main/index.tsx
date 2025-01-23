30c4b9002c83a32a72cfcb7b846dccb64c136650
IlhanCodeAc <ilhanma@code.edu.az>
2025-01-24 01:32:15 +0400

    fix: error handling

--- Client/src/Pages/Home/Parts/Main/index.tsx
+++ Client/src/Pages/Home/Parts/Main/index.tsx
@@ -7,7 +7,7 @@ const HomeMain = () => {
     <div>
         <div className={style.HomeContainer}>
             <div className={style.Left}>
-                <h3 className="text-[64px] font-sans font-semibold leading-none text-[#042352] pt-[56px]">Tech Heims</h3>
+                <h3 className="text-[64px] font-sans font-semibold leading-none text-[#042352] pt-[56px]">Tech Heim</h3>
                 <div className="flex mt-[50px]"><p className='flex mx-[5px] text-[32px] font-sans font-medium leading-none text-[#042352]'>"Join the</p><p className='text-[32px] font-sans font-medium leading-none text-[#F45E0C]'>digital revolution</p><p className='flex mx-[5px] text-[32px] font-sans font-medium leading-none text-[#042352]'>"</p></div>
                 <button className='mt-[128px] flex w-[288px] h-[56px] p-[8px_16px] justify-center items-center gap-[8px] flex-shrink-0 rounded-[8px] bg-[#F45E0C] text-white text-center text-[16px] font-sans font-normal leading-none'>Explore More</button>
             </div>
